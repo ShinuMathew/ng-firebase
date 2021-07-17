b1529193d5f6a5f2d1890c0426b7693e8e1d7c88
Shinu Mathew <shinu.mathew@samsung.com>
2021-07-17 15:36:55 +0530

    Added loading spinner

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -10,16 +10,20 @@ export class SignupComponent implements OnInit {
 
   @Output('is-loggedin')
   public isLoggedIn: EventEmitter<boolean> = new EventEmitter();
-  public hide : boolean = true;
 
+  public hide : boolean = true;
+  public loading : boolean = false;
   constructor(public firebaseService : FirebaseService) { }
 
   ngOnInit(): void {
+    this.loading = false;
   }
 
   async onSignUp(email : string, password : string, firstName? : string, lastName? : string, phoneNo? : number) {
+    this.loading = true;
     await this.firebaseService.signUpAsNewUser(email, password, firstName, lastName, phoneNo);
     this.isLoggedIn.emit();
+    this.loading = false;
   }
 
   showPassword() {
